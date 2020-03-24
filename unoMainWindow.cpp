c8f00ef7dde027576b57cbc6347501fd01a6838f
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-03-24 11:13:38 +0300

    Generation of libreoffice include files using cppumaker.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- unoMainWindow.cpp
+++ unoMainWindow.cpp
@@ -8,21 +8,19 @@
 #include "ui_uno_main_window.h"
 
 #include <sal/main.h>
-/*
+
 #include <cppuhelper/bootstrap.hxx>
-*/
+
 #include <osl/file.hxx>
 #include <osl/process.h>
 #include <rtl/process.h>
 
-/*
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/bridge/XUnoUrlResolver.hpp>
 #include <com/sun/star/frame/Desktop.hpp>
 #include <com/sun/star/frame/XComponentLoader.hpp>
 #include <com/sun/star/lang/XMultiComponentFactory.hpp>
 #include <com/sun/star/registry/XSimpleRegistry.hpp>
-*/
 
 UnoMainWindow::UnoMainWindow(QWidget *parent)
     : QMainWindow(parent),
