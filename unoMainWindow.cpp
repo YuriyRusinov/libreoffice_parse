0cbb740a3dda013e397647d50ff7e9b2af591efa
Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>
2020-03-25 17:25:30 +0300

    Link with libreoffice libraries successful.
    Signed-off-by: Yuriy Rusinov <Y.L.Rusinov@rubin-spb.ru>

--- unoMainWindow.cpp
+++ unoMainWindow.cpp
@@ -1,7 +1,10 @@
 #include <QDir>
 #include <QMdiArea>
 #include <QFileDialog>
+#include <QUrl>
 #include <QtDebug>
+
+#include <sstream>
 
 #include "unoFileWidget.h"
 #include "unoMainWindow.h"
@@ -14,6 +17,8 @@
 #include <osl/file.hxx>
 #include <osl/process.h>
 #include <rtl/process.h>
+#include <rtl/string.h>
+#include <rtl/ustrbuf.hxx>
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/bridge/XUnoUrlResolver.hpp>
@@ -21,6 +26,18 @@
 #include <com/sun/star/frame/XComponentLoader.hpp>
 #include <com/sun/star/lang/XMultiComponentFactory.hpp>
 #include <com/sun/star/registry/XSimpleRegistry.hpp>
+
+using namespace com::sun::star::uno;
+using namespace com::sun::star::lang;
+using namespace com::sun::star::beans;
+using namespace com::sun::star::bridge;
+using namespace com::sun::star::frame;
+using namespace com::sun::star::registry;
+
+using ::rtl::OUString;
+using ::rtl::OUStringBuffer;
+using ::rtl::OUStringToOString;
+using std::stringstream;
 
 UnoMainWindow::UnoMainWindow(QWidget *parent)
     : QMainWindow(parent),
@@ -45,10 +62,14 @@ void UnoMainWindow::slotOpen() {
                 tr ("Open File"),
                 QDir::currentPath(),
                 tr ("Open Document Text Files (*.odt)"));
-    qDebug() << __PRETTY_FUNCTION__ << fileName;
     if (fileName.isEmpty())
         return;
 
+    QUrl fileUrl=QUrl::fromLocalFile(fileName);
+    qDebug() << __PRETTY_FUNCTION__ << fileUrl << fileUrl.toString().length();
+    OUString sConnectionString("uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager");
+
+    OUStringBuffer buf (fileUrl.toString().length());
     unoFileWidget* w = new unoFileWidget;
     w->setAttribute(Qt::WA_DeleteOnClose);
     QMdiSubWindow * subW = _mdiArea->addSubWindow(w);
